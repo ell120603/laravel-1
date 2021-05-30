3dcdc0215f02bdcbf070db118e73433745b6efba
Eliel Camara <eliel.camara@etec.sp.gov.br>
2021-05-30 16:41:38 -0300

    1.0

---
+++ resources/views/first.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Primeira Pagina laravel</title>
+</head>
+<body>
+<h1>Primeira Tela no laravel</h1>
+<form class="teste" > 
+            <input type="text" name="nome" placeholder="Digite seu nome" required>
+            <input type="text" name="idade" placeholder="Digite sua idade" required>
+            <div class="flex">
+                <input class="btn" type="submit" value="Enviar" required>
+            </div>
+</body>
+</html>
